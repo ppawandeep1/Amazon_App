4f1deb390661822dddb044ea42fa966e60da2bd5
krishna97p <kpanchal@isi-mtl.com>
2020-03-23 14:06:36 -0400

    Added volley library and async task library by using it we can directly fetch data and added the onclick listener

---
+++ app/src/main/java/com/example/amazonapp/Helper/Config.java
@@ -0,0 +1,15 @@
+package com.example.amazonapp.Helper;
+
+/**
+ * Created by admin on 08/11/2017.
+ */
+
+public class Config {
+  // public static final String MAIN_URL = "http://development.ifuturz.com/core/FLAT_TEST/ecart_new/admin/webservice.php";
+    public static final String MAIN_URL = "http://192.168.43.70/Food/api/Food/loginUser";
+  public static final String HOMEPAGE_URL="http://192.168.43.70/Food/api/Food/GetDailyMenu";
+  public static final String WALLET_URL="http://192.168.43.70/Food/api/Food/GetWalletMaster";
+  public static final String ORDER_URL="  http://192.168.43.70/Food/api/Food/PutOrderMaster";
+  public static final String GET_CATEGORIES="  http://18.218.124.225:3000/api/category/getcategories";
+
+}
